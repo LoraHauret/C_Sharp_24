5bd1ef0a86b8388a0d974d049af3331ecfb4078c
LoraHauret <117381884+LoraHauret@users.noreply.github.com>
2025-04-09 00:10:24 +0300

    Add files via upload

---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,52 @@
+﻿/*
+ 
+2. Практика на методы расширения (extenstion)
+class Program
+{
+static void Main(string[] args)
+{
+"Hello world".Print();
+2024.Print();
+0.08.Print();
+}
+}
+сделать так, чтобы этот код скомпилировался
+ */
+
+namespace ExtensionMethods
+{
+    public static class StringExtensions
+    {
+        public static string Print(this string str)
+        {
+            Console.WriteLine("Строка печатается в расширении: " + str);
+            return str;
+        }
+    }
+    public static class IntExtensions
+    {
+        public static int Print(this int number)
+        {
+            Console.WriteLine("Целое число печатается в расширении: " + number);
+            return number;
+        }
+    }
+    public static class DoubleExtensions
+    {
+        public static double Print(this double number)
+        {
+            Console.WriteLine("Число печатается в расширении: " + number);
+            return number;
+        }
+    }
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            "Hello, World!".Print();
+            2024.Print();
+            0.08.Print();
+        }
+    }
+}
